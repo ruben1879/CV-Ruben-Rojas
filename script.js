f12ba10625786c8117b12b8ce01ac7bdd2104b47
Ruben <ruben.rojas79@gmail.com>
2023-02-27 15:10:13 -0300

    Agrego imagenes de fondo

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('mostrar-imagen').addEventListener('click', function () {
+  document.getElementById('imagen').style.visibility = 'auto';
+});
+document.getElementById('ocultar-imagen').addEventListener('click', function () {
+  document.getElementById('imagen').style.display = 'none';
+});
