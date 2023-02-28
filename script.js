d71650c429b33373ae1aa0e463892e52706bffd2
Ruben <ruben.rojas79@gmail.com>
2023-02-28 00:35:08 -0300

    Claculo de edad en javaScript

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.getElementById('mostrar-imagen').addEventListener('click', function () {
-  document.getElementById('imagen').style.visibility = 'auto';
+  document.getElementById('imagen').style.visibility = 'visible';
 });
 document.getElementById('ocultar-imagen').addEventListener('click', function () {
-  document.getElementById('imagen').style.display = 'none';
+  document.getElementById('imagen').style.visibility = 'hidden';
+
+});
+
+document.getElementById('calcular-edad').addEventListener('click', function (){
+const fecha= new Date();
+let fechaActual= 0;
+let fechaNacimiento= 0;
+
+fechaActual= fecha.getFullYear();
+fechaNacimiento= 1979;
+
+let resultado= fechaActual - fechaNacimiento;
+
+console.log(resultado)
+document.getElementById('resultado').innerHTML = resultado + " Años";
 });
